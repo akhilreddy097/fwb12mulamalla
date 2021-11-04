2c65d0d9aa4025db0eb9084a17e9581e92f7d389
akhilreddy097 <akhilreddym970@gmail.com>
2021-11-04 01:12:59 -0500

    added all files and images

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var shirtRouter = require('./routes/shirt');
+var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
+
 var app = express();
 
 // view engine setup
@@ -22,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/shirt', shirtRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/shirt.js
+++ routes/shirt.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+class shirt {
+  constructor(shirt_color, shirt_brand,shirt_cost) {
+    this.shirt_color = shirt_color;
+    this.shirt_brand = shirt_brand;
+    this.shirt_cost = shirt_cost;
+  }
+}
+
+/* GET shirt page. */
 router.get('/', function(req, res, next) {
-  res.render('shirt', { title: 'shirt Search Results' });
+  const shirt1 = new shirt('blue', "nike", '10.29');
+  const shirt2 = new shirt('red', "holister", '20.11');
+  const shirt3 = new shirt('green', "puma", '40.12');
+  res.render('shirt', {shirt : [shirt1, shirt2, shirt3]});
 });
 
 module.exports = router;
